91b75074c047de537fd68d0e0cdf400045a48bb2
root <root@cimhe.com>
2018-03-13 00:11:28 +0000

    Online Fixe

--- resources/views/web/blog/blog.blade.php
+++ resources/views/web/blog/blog.blade.php
@@ -60,7 +60,7 @@
                                 </span>Etiquetas: 
                                 @foreach($article->tags as $tag)
                                     <a href="{{ route('web.search.tag', $tag->name ) }}">
-                                        <span>{!! $tag->name !!}</span>xxx
+                                        <span>{!! $tag->name !!}</span>
                                     </a>
                                 @endforeach
                                 @endif
