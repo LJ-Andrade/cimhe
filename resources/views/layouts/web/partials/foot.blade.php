a265435a92cfea4d7e5fdecb41e849eb1834944e
javzero <javzero@hotmail.com>
2018-03-14 19:27:06 -0300

    Gallery Fixes

--- resources/views/layouts/web/partials/foot.blade.php
+++ resources/views/layouts/web/partials/foot.blade.php
@@ -3,14 +3,13 @@
 		<ul>
 			<li><a href="https://www.facebook.com/CIMHE-SRL-1915074345372139/" target="_blank">
 				<img src="{{ asset('webimages/main/redes1.png') }}" alt=""></li></a>
-			<li><a href="Instagram: https://www.instagram.com/cimhesrl/
-" target="_blank">
+			<li><a href="https://www.instagram.com/cimhesrl/" target="_blank">
 				<img src="{{ asset('webimages/main/redes2.png') }}" alt=""></li></a>
 			<li><a href="#" target="_blank">
 				<img src="{{ asset('webimages/main/redes3.png') }}" alt=""></li></a>
 			<li><a href="https://twitter.com/cimhe_srl" target="_blank">
 				<img src="{{ asset('webimages/main/redes4.png') }}" alt=""></li></a>
-			<li><a href="Youtube: https://www.youtube.com/channel/UCo5Y2yWCC8L9fFEFN_o9fzQ" target="_blank">
+			<li><a href="https://www.youtube.com/channel/UCo5Y2yWCC8L9fFEFN_o9fzQ" target="_blank">
 				<img src="{{ asset('webimages/main/redes5.png') }}" alt=""></li></a>
 		</ul>
 	</div>
